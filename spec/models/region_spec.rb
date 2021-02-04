650a964953b1c85694729db291c54f1cbfd6fa0b
Danielle Puga <danielle.puga@gmail.com>
2021-02-03 17:12:03 -0800

    started working on goal 2 - initialization tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  let(:valid_region) {Region.new(name: 'Fake region')}
+
+  it 'exists' do
+    Region.new
+  end
+
+  it 'has a name' do 
+    region = Region.new
+    expect(region).to respond_to(:name)
+  end
+
+  it 'is not valid without a name' do
+    region = Region.new(name: 'Fake region')
+    expect(region).to be_valid
+    region.name = nil
+    expect(region).to_not be_valid
+  end
+
 end
