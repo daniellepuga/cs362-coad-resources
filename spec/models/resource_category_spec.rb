f7b9882def072609b9625c2f3c5ec83d006f85fe
Danielle Puga <danielle.puga@gmail.com>
2021-02-03 17:20:30 -0800

    began resource_category spec

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  let (:resource_category) do
+    ResourceCategory.new
+  end
+
+  it 'has a name' do
+    expect(resource_category).to respond_to(:name)
+  end
+
+  it 'has an active status' do
+    expect(resource_category).to respond_to(:active)
+  end
 
 end
+
