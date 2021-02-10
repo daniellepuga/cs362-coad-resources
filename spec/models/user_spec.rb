2c6911e3936a38cb3a0fd14a63373fd4a8abe817
Danielle Puga <danielle.puga@gmail.com>
2021-02-09 21:03:01 -0800

    fixed user spec to match instructions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,20 +9,8 @@ RSpec.describe User, type: :model do
     expect(user).to respond_to(:role)
   end
 
-  it 'has an organization' do
-    expect(user).to respond_to(:organization)
-  end
-
   it 'has an email' do
     expect(user).to respond_to(:email)
-  end
-
-  it 'has a password' do
-    expect(user).to respond_to(:password)
-  end
-
-  it 'has a default role' do
-    expect(user).to respond_to(:set_default_role)
   end
 
 end
