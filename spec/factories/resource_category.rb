4313eb6502f09287193b86f6044dc2e6b1e53e60
Danielle Puga <danielle.puga@gmail.com>
2021-03-02 13:47:45 -0800

    resource category factory added

--- spec/factories/resource_category.rb
+++ spec/factories/resource_category.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :resource_category do
     name { 'Fake' }
+    trait :unspecified do
+      name { 'Unspecified' }
+    end
+    trait :active do
+      active { true }
+    end
+    trait :inactive do
+      active { false }
+    end
   end
 end
