ca99c1ab7e6265704587cdf6e33694bc680f8350
Danielle Puga <danielle.puga@gmail.com>
2021-03-02 23:32:59 -0800

    starting controller tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  it 'returns http success' do
+    get :index
+    expect(response).to have_http_status(:success)
+  end
 end
