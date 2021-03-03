2394f9d6b925bfbbb0771c5e9fe1ac8fcd167311
Danielle Puga <danielle.puga@gmail.com>
2021-03-03 13:27:35 -0800

    following format provided in lecture 10 for controllers

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  context 'unauthenticated users' do
+    describe '#index' do
+      it 'redirects to the sign_in screen' do
+        get :index
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+  describe '#show' do
+    it 'redirects to the sign_in screen' do
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_url)
+    end
+  end
+
+  describe '#new' do
+    it 'redirects to the sign_in screen' do
+      get :new
+      expect(response).to redirect_to(new_user_session_url)
+    end
+  end
+
+  describe '#create' do
+    it 'redirects to the sign_in screen' do
+      post :create
+      expect(response).to redirect_to(new_user_session_url) 
+    end
+  end
+end
 end
