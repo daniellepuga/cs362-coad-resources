9012583472b0ddd53bb08cc9d3fe135375e79f79
Danielle Puga <danielle.puga@gmail.com>
2021-03-09 10:22:10 -0800

    modified ticket factory and spec

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,57 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  context 'unauthenticated users' do
+    it 'handles different situations for tickets' do
+      get :new
+      expect(response).to be_successful
+      post :capture, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      post :release, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      delete :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+    # describe 'creates new' do
+    #   specify { 
+    #     ticket = create(:ticket)
+    #     expect(post(:create, params: { id: ticket.id, ticket: attributes_for(:ticket) })).to be_successful
+    #   }
+    # end
+
+  context 'organization users' do
+    let(:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    it 'redirects to the dashboard' do
+      get :new
+      expect(response).to be_successful
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+    # describe 'creates a post' do
+    #   specify { 
+    #     ticket = create(:ticket)
+    #     expect(post(:create, params: { id: ticket.id, ticket: attributes_for(:ticket) })).to be_successful
+    #   }
+    # end
+
+  class FakeTicket
+    def get_ticket
+      :ok
+    end
+
+    def release_ticket
+      :ok
+    end
+
+    def close_ticket
+      :ok
+    end
+  end
 end
+
--- spec/factories/ticket.rb
+++ spec/factories/ticket.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :ticket do
     name { 'Fake' }
     description { 'Fake Description' }
-    phone { '555-123-1234' }
-    region { create(:region).id }
-    resource_category { create(:resource_category).id }
-
+    phone { '+15414541232' }
+    region_id { create(:region).id }
+    resource_category_id { create(:resource_category).id }
+    # id {}
     trait :open do
       closed { false }
     end
