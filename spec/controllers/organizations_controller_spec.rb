ff480164bd7c7213c833a961013a3646afc2ee3f
Danielle Puga <danielle.puga@gmail.com>
2021-03-14 15:59:37 -0700

    progress on organization controller tests

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -24,30 +24,47 @@ RSpec.describe OrganizationsController, type: :controller do
       expect(response).to redirect_to(new_user_session_url)
     end
   end
+
+  class FakeMailer
+    def new_organization_application
+      false
+    end
+  end
   
-  context 'organization users' do
-    before do
-      reg_usr = build(:user)
-      allow(request.env['warden']).to receive(:authenticate!).and_return(reg_usr)
-      allow(controller).to receive(:current_user).and_return(reg_usr)
+  context 'users' do
+    let(:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    it 'redirects to the dashboard' do
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      patch :update, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      post :approve, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      post :reject, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+      get :new
+      expect(response).to be_successful
+      get :index
+      expect(response).to be_successful 
+      post :create, params: { organization: {name: 'fake'} }
+      expect(response).to be_successful
     end
 
-    # it 'redirects to the dashboard' do
-    #   get :show, params: {id: 'fake'}
-    #   expect(response).to be_successful
-    #   get :new
-    #   expect(response).to be_successful
-    #   post :create
-    #   expect(response).to be_successful 
-    #   patch :update, params: {id: 'fake'}
-    #   expect(response).to be_successful 
-    #   get :edit, params: {id: 'fake'}
-    #   expect(response).to be_successful 
-    #   patch :update, params: {id: 'fake'}
-    #   expect(response).to be_successful
-    #   put :update, params: {id: 'fake'}
-    #   expect(response).to be_successful
-    # end
+    it 'redirects to a path after creation' do
+      allow(UserMailer).to receive(:with).and_return FakeMailer.new
+      expect(post(:create, params: { organization: attributes_for(:organization) })).to redirect_to(organization_application_submitted_path)
+    end
+
+    describe 'it can get edit' do
+      specify {
+        user.organization = create(:organization, :approved)
+        user.save
+        expect(get(:edit, params: {id: user.organization.id} )).to be_successful
+      }
+    end
+
   end
       
 
--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+    # let(:admin) { create(:user, :admin) }
+
+    # it 'succeeds' do 
+    #     region_name = 'Fake Region'
+    #     log_in_as(admin)
+    #     click_on 'Regions'
+    #     click_on 'Add Region'
+    #     fill_in 'Name', with: 'Fake Region'
+    #     click_on 'Add Region'
+    #     expect(page.body).to have_text('Region successfully created')
+    #     expect(page.body).to have_text(region_name)
+    # end
+    
+    # it 'fails when the name is missing' do 
+    #     log_in_as(admin)
+    #     click_on 'Regions'
+    #     click_on 'Add Region'
+    #     fill_in 'Name', with: '' 
+    #     click_on 'Add Region'
+    #     expect(page.body).to have_text('errors')
+    # end  
 end
